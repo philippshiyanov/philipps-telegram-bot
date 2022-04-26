a7732b7794fa84e1fa3a9c36247d1cfdfe8c3fd5
Phil <phil@romans-macbook-pro.local>
2022-04-26 12:01:07 +0000

    upd

--- telegram.js
+++ telegram.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const bot = window.Telegram.WebApp;
   bot.ready();
-  bot.MainButton.text("Click here!");
+  bot.MainButton.text = "Click Here!";
   bot.show();
 });
