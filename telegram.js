0ac20490e136221e9f1c4e185c2ef16810161fa6
Phil <phil@romans-macbook-pro.local>
2022-04-26 17:37:41 +0000

    upd

--- telegram.js
+++ telegram.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const bot = window.Telegram.WebApp;
   bot.ready();
-  bot.expand(); //works
+  bot.MainButton.text = "hello";
+  bot.InlineKeyboardButton =
+    "https://philippshiyanov.github.io/philipps-telegram-bot/";
   bot.MainButton.setText("Here!"); //doesnt work
   bot.MainButton.onClick(() => {
     bot.sendData("1");
   });
+  bot.expand(); //works
 });
