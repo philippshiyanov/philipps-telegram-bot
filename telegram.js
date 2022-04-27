352be9fd94b01dc0cfbd868b5eeca7122b53f59b
Phil <phil@romans-macbook-pro.local>
2022-04-27 10:37:41 +0000

    upd

--- telegram.js
+++ telegram.js
@@ -7,14 +7,28 @@ document.addEventListener("DOMContentLoaded", () => {
       cart.push(e.currentTarget.parentNode.id);
       console.log(cart);
       bot.MainButton.setText("Checkout");
-      bot.MainButton.show(); //swapped
+      bot.MainButton.show();
+      bot.MainButton.enable(); //swapped
     })
   );
 
   bot.MainButton.OnClick(() => {
     document.getElementById("cart").style.display = "block";
     document.getElementById("store").style.display = "none";
+    bot.MainButton.setText("onclick");
   });
+
+  Telegram.WebApp.onEvent("mainButtonClicked", () => {
+    document.getElementById("cart").style.display = "block";
+    document.getElementById("store").style.display = "none";
+    bot.MainButton.setText("onEvent");
+  });
+  function clicked() {
+    document.getElementById("cart").style.display = "block";
+    document.getElementById("store").style.display = "none";
+    bot.MainButton.setText("onEventNoParams");
+  }
+  Telegram.WebApp.onEvent("mainButtonClicked", clicked());
 
   bot.ready();
   bot.expand(); //works
