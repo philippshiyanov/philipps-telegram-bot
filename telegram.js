d94b65fb4555d6f8e44f80f64d44745c743cf159
Phil <phil@romans-macbook-pro.local>
2022-04-30 14:27:10 +0000

    cart items update

--- telegram.js
+++ telegram.js
@@ -1,16 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
   const bot = window.Telegram.WebApp;
-  const cart = [];
+  let cart = [];
   let cartClick = document.getElementById("cart");
   let storeClick = document.getElementById("store");
   bot.ready();
 
-  document.querySelectorAll("button").forEach((btn) =>
+  const addBtn = document.querySelectorAll("#add");
+  const plus = document.querySelectorAll("#plus");
+  const minus = document.querySelectorAll("#minus");
+  const minus_plus = document.querySelectorAll("#minus-plus");
+  const counter = document.querySelectorAll(".count");
+  addBtn.forEach((btn) =>
     btn.addEventListener("click", (e) => {
-      cart.push(e.currentTarget.parentNode.id);
+      let itemID = e.currentTarget.parentNode.id;
+      cart.push(itemID);
       console.log(cart);
       bot.MainButton.setText("Checkout");
       bot.MainButton.show();
+      //shows the - + buttons
+      addBtn[itemID - 1].style.display = "none";
+      minus_plus[itemID - 1].style.display = "block";
+      //item counter
+      counter[itemID - 1].style.display = "block";
+      counter[itemID - 1].children[0].innerHTML = cart.filter(
+        (x) => x == itemID
+      ).length;
+    })
+  );
+
+  plus.forEach((plus) =>
+    plus.addEventListener("click", (e) => {
+      let itemID = e.currentTarget.parentNode.parentNode.id;
+      cart.push(itemID);
+      //update counter
+      counter[itemID - 1].children[0].innerHTML = cart.filter(
+        (x) => x == itemID
+      ).length;
+      console.log(cart);
+    })
+  );
+
+  minus.forEach((minus) =>
+    minus.addEventListener("click", (e) => {
+      let itemID = e.currentTarget.parentNode.parentNode.id;
+      //deletes selected element from cart array
+      cart.splice(
+        cart.findIndex((index) => index === itemID),
+        1
+      );
+
+      //if array doesn't contain element then the minus button is hidden
+      if (cart.findIndex((index) => index === itemID) == -1) {
+        addBtn[itemID - 1].style.display = "block";
+        minus_plus[itemID - 1].style.display = "none";
+      }
+      //clear counter if 0
+      if (cart.filter((x) => x == itemID).length == 0) {
+        counter[itemID - 1].style.display = "none";
+      } else {
+        counter[itemID - 1].children[0].innerHTML = cart.filter(
+          (x) => x == itemID
+        ).length;
+      }
+
+      console.log(cart);
     })
   );
 
@@ -35,12 +88,6 @@ document.addEventListener("DOMContentLoaded", () => {
     storeClick.style.display = "none";
     bot.MainButton.hide();
   });
-  // .then(() => {
-  //   Telegram.WebApp.offEvent("mainButtonClicked", () => {
-  //     cartClick.style.display = "none";
-  //     storeClick.style.display = "grid";
-  //     Telegram.WebApp.hide();
-  //   });
-  // });
+
   bot.expand();
 });
